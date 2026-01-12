f0bdf66c7ff406791b5bd2aed43e4287d971a2fc
DL21hex <diego.llinas@gmail.com>
2026-01-12 16:00:36 -0500

    404 creado

--- src/routes/[[...slug]].tsx
+++ src/routes/[[...slug]].tsx
@@ -23,9 +23,28 @@ const fetchContent = async (path: string) => {
 
 function NotFound() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Página no encontrada</h1>
-    </main>
+    <div data-slot="empty" class="flex min-w-0 flex-1 flex-col items-center justify-center gap-6 rounded-lg border-dashed p-6 text-center text-balance md:p-12">
+      <div data-slot="empty-header" class="flex max-w-sm flex-col items-center gap-2 text-center">
+        <div data-slot="empty-icon" data-variant="icon" class="mb-2 [&_svg]:pointer-events-none [&_svg]:shrink-0 bg-muted text-foreground flex size-10 shrink-0 items-center justify-center rounded-lg [&_svg:not([class*='size-'])]:size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-folder-code ">
+            <path d="M11 19h-6a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v4"></path>
+            <path d="M20 21l2 -2l-2 -2"></path>
+            <path d="M17 17l-2 2l2 2"></path>
+          </svg>
+        </div>
+        <div data-slot="empty-title" class="text-lg font-medium tracking-tight">Página no encontrada</div>
+        <div data-slot="empty-description" class="text-muted-foreground [&>a:hover]:text-primary text-sm/relaxed [&>a]:underline [&>a]:underline-offset-4">
+          La página que buscas no existe o ha ocurrido un error.
+        </div>
+      </div>
+      <div data-slot="empty-content" class="flex w-full max-w-sm min-w-0 flex-col items-center gap-4 text-sm text-balance">
+        <div class="flex gap-2">
+          <a href="/" data-slot="button" data-variant="default" data-size="default" class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 has-[>svg]:px-3">
+            Volver al inicio
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
